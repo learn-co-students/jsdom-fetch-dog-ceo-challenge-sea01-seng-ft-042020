e1f1a9cdddefe442554e1213b551f5e782d59273
ckaiser258 <kaiserc2@seattleu.edu>
2020-06-04 14:11:26 -0700

    challenge 2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ console.log('%c HI', 'color: firebrick')
 document.addEventListener("DOMContentLoaded", function(){
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
     const imgList = document.querySelector("#dog-image-container")
-
-    console.log(imgList)
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    const breedList = document.getElementById("dog-breeds")
 
     function fetchDogs () {
         fetch(imgUrl)
@@ -18,5 +18,19 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         })
     }
+    function fetchBreeds() {
+        fetch(breedUrl)
+        .then(function(response) {
+            return response.json()
+        }) .then(function(json) {
+            for ( i in json.message){
+            // "in" because this is an object not an array
+                const list = document.createElement("li");
+                list.innerText = i;
+                breedList.appendChild(list);
+            }
+        })
+    }
     fetchDogs()
+    fetchBreeds()
 })
