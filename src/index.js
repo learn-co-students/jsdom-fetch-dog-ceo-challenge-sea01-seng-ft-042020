fc394d869487133a13fe40308de284fe3baef3c0
ckaiser258 <kaiserc2@seattleu.edu>
2020-06-04 15:06:29 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
 
 document.addEventListener("DOMContentLoaded", function () {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
