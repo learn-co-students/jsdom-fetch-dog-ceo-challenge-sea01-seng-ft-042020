b06c3d802d56a56c979c1c0afef9c4caaeadd87c
aidanmc95 <aidanmc95@gmail.com>
2020-06-04 13:55:53 -0700

    challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    const imgList = document.querySelector("#dog-image-container")
+
+    console.log(imgList)
+
+    function fetchDogs () {
+        fetch(imgUrl)
+        .then(function(response) {
+            return response.json()
+        }) .then(function(json) {
+            for (i of json.message){
+                const img = document.createElement("img");
+                img.src = i;
+                imgList.appendChild(img);
+            }
+        })
+    }
+    fetchDogs()
+})
